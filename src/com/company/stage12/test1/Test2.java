8d67442f4b82e2b43c52a83b44406d5a3486e508
yangdiTop <1289331481>
2018-09-09 15:45:58 +0800

    stage12 : 修改 Test2.java的代码 演示新内容

--- src/com/company/stage12/test1/Test2.java
+++ src/com/company/stage12/test1/Test2.java
@@ -3,5 +3,6 @@ package com.company.stage12.test1;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("演示覆盖远程代码");
+        System.out.println("演示新内容");
     }
 }
