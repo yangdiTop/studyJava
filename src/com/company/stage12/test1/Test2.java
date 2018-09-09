7a19eefa54385a69137db633325155d792056921
yangdiTop <1289331481>
2018-09-09 15:44:51 +0800

    stage12 : 修改 Test2.java的代码 演示远程代码被替换

--- src/com/company/stage12/test1/Test2.java
+++ src/com/company/stage12/test1/Test2.java
@@ -2,6 +2,6 @@ package com.company.stage12.test1;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("演示git提交代码");
+        System.out.println("演示覆盖远程代码");
     }
 }
